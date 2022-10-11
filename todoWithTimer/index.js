6e21e24d3327491096a89712e6cb6d1e5354cc3d
Therese Weidenstedt <kw222mi@student.lnu.se>
2022-10-11 14:40:14 +0200

    function add todo

--- todoWithTimer/index.js
+++ todoWithTimer/index.js
@@ -1,15 +1,28 @@
 import './style.css'
-import './src/components/todo'
+import Todo from './src/components/Todo.js'
+import Timer from '/src/components/Timer.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
   
-  
-    <div >
-      
+ 
+    <div>
+     
     </div>
    
   </div>
 `
+let todo = new Todo ()
+
+let timerDiv = document.querySelector('#timerDiv')
+
+let timer = new Timer ({
+  displayElement: timerDiv,
+  timerTime: 30,
+  showProgressBar: true,
+  pauseOnHover: true,
+  timeIsUpAction: 'color',
+  tenSecondsLeftWarning: true
+})
 
 
---
+++ todoWithTimer/src/components/todo.js
@@ -0,0 +1,67 @@
+
+/**
+ *
+ */
+export default class Todo {
+  #todoDiv
+  #newTodo
+
+  #todoInput = document.querySelector('.todo-input')
+  #todoButton = document.querySelector('.todo-button')
+  #todoList = document.querySelector('.todo-list')
+
+  /**
+   *
+   */
+  constructor () {
+   this.#setEventListeners()
+  }
+  
+  /**
+   *
+   */
+  #setEventListeners () {
+    this.#todoButton.addEventListener('click', (event) => { this.#doTodo(event) })
+    this.#todoInput.addEventListener('keydown', (event) => console.log(event.target.value))
+    // this.#todoList.addEventListener("click", this.#deleteTodo)
+  }
+
+  /**
+   *
+   * @param e
+   */
+  #doTodo (event) {
+    event.preventDefault()
+    console.log('dotodo')
+    this.#createTodoDiv()
+    this.#createTodoList(event)
+    this.#addTodo()
+    this.#todoList.appendChild(this.#todoDiv)
+  }
+
+  /**
+   *
+   */
+  #createTodoDiv () {
+    this.#todoDiv = document.createElement('div')
+    this.#todoDiv.classList.add('todo')
+  }
+
+  /**
+   *
+   */
+  #createTodoList (event) {
+    this.#newTodo = document.createElement('li')
+    this.#newTodo.innerText = this.#todoInput.value
+  }
+
+  /**
+   *
+   */
+  #addTodo () {
+    this.#newTodo.classList.add('todo-item')
+    this.#todoDiv.appendChild(this.#newTodo)
+    this.#todoInput.value = ''
+  }
+
+}
